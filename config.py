062f97637fa6aaa1982902fde4b484247fe5d548
Swadheen Prusty <swadheenrinku@gmail.com>
2022-09-10 18:41:14 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+CSV_FILE_PATH = " enter file path"
+
+PORT_NUMBER =  5005
+
+MODEL_FILE_PATH = os.path.join('Heart_Data','knn_clf_hyp.pkl')
+
+JSON_FILE_PATH =  os.path.join('Heart_Data','heart_data.json')
